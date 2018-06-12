df83d29945d1a4f20f613341864c3ee3870ddbcc
Yan Mendes <yan.mendes@planejarjf.com.br>
2018-06-12 14:07:34 -0300

    Atualizacao do script

--- charts/all.py
+++ charts/all.py
@@ -3,10 +3,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 
-#GENERATOR FOR COMPARISON
+#GENERATOR FOR COMPARISON PEMS
 
 if(True):
 	comp = pandas.read_csv('comp_pems.csv', header=0)
+
+	X = range(0, len(comp['LOGI']))
+	nn = list()
+	rbm = list()
+	relu = list()
+
+	for i in xrange(0, len(comp['LOGI']), 10):
+		nn.append(np.mean(comp['LOGI'][i:i+10]))
+		relu.append(np.mean(comp['RELU'][i:i+10]))
+		rbm.append(np.mean(comp['RBM'][i:i+10]))
+
+	f, (ax1, ax2) = plt.subplots(1, 2)
+	ax1.set_title('All evaluations')
+	ax2.set_title('Average evaluation')
+	ax1.set_xlabel('Test #')
+	ax1.set_ylabel('MAPE')
+	ax2.set_xlabel('Test #')
+	ax1.scatter(X, comp['LOGI'], color='blue', marker='x', alpha=0.4, label='ST (Logistic)')
+	ax1.scatter(X, comp['RELU'], color='#c8f442', marker='+', alpha=0.1, label='ST (ReLU)')
+	ax1.scatter(X, comp['RBM'], color='red', marker='v', alpha=0.3, label='Huang et. al')
+	ax2.scatter(range(0, len(nn)), nn, color='blue', marker='x', alpha=0.5, label='ST (Logistic)')
+	ax2.scatter(range(0, len(relu)), relu, color='#c8f442', marker='+', alpha=0.1, label='ST (ReLU)')
+	ax2.scatter(range(0, len(rbm)), rbm, color='red', marker='v', alpha=0.5, label='Huang et. al')
+	ax2.legend()
+	ax1.legend()
+
+	f.savefig('comp_pems.eps')
+
+#GENERATOR FOR COMPARISON UNIVERSITY
+
+if(False):
+	comp = pandas.read_csv('comp.csv', header=0)
 
 	X = range(0, len(comp['LOGI']))
 	nn = list()
